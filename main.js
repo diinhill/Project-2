c42c7b91c239f4408baaf2d71abe03e1c49769ec
diinhill <hillenbrand.nadine@hotmail.de>
2021-07-22 22:53:20 +0200

    checkbox menu

--- main.js
+++ main.js
@@ -238,21 +238,3 @@ function warp() {
     setTimeout(smallUfo, 6000)
 }
 
-$(function() {
-    $("#from").datepicker({
-        defaultDate: "+1w",
-        changeMonth: true,
-        numberOfMonths: 1,
-        onClose: function(selectedDate) {
-            $("to").datepicker("option", "minDate", selectedDate)
-        }
-    })
-    $("#to").datepicker({
-        defaultDate: "+1w",
-        changeMonth: true,
-        numberOfMonths: 1,
-        onClose: function(selectedDate) {
-            $("from").datepicker("option", "maxDate", selectedDate)
-        }
-    })
-})
